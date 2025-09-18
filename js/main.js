5e678086d17e7a51aff5a9d4ba7f8801e34b8ed6
Vishnu Rangadass <vishnurangadass@gmail.com>
2025-09-18 15:49:26 +0530

    second commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const arr = [
+  "Thalapathy",
+  "Virumandi",
+  "Ghilli",
+  "Mankatha",
+  "Ayan",
+  "Anniyan",
+  "Amaran",
+  "Maharaja",
+  "Kaithi",
+  "Thanioruvan",
+];
+
+arr.forEach((movie)=>{
+    console.log(movie)
+})
